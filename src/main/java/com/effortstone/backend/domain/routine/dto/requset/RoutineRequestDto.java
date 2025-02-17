dd2511011efcd8ae6d98b87224d26da01727a1a7
Wonhee <totohwh3601@naver.com>
2025-02-17 15:31:58 +0900

    Chore - Dto 데이터 타입 수정

--- src/main/java/com/effortstone/backend/domain/routine/dto/requset/RoutineRequestDto.java
+++ src/main/java/com/effortstone/backend/domain/routine/dto/requset/RoutineRequestDto.java
@@ -1,6 +1,7 @@
 package com.effortstone.backend.domain.routine.dto.requset;
 
 import com.effortstone.backend.domain.routine.entity.RoutineGoalType;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -17,14 +18,18 @@ public class RoutineRequestDto {
     public static class RoutineCreateRequest {
         private String routineName;
         private RoutineGoalType routineGoalType;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
         private LocalTime routineFocusTime;
         private List<Integer> routineRepeatFrequency;
         private LocalDate routineStartDate;
         private LocalDate routineEndDate;
         private String routineTheme;
         private String routineDetail;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
         private LocalTime routineStartTime;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
         private LocalTime routineEndTime;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
         private LocalTime routineAlertTime;
     }
 }
