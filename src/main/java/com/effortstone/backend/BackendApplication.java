547a2e8f1926bbfc66a28c533f0c492b7d5f9586
Wonhee <totohwh3601@naver.com>
2025-02-17 14:03:29 +0900

    Chore - 간단한 주석 추가

--- src/main/java/com/effortstone/backend/BackendApplication.java
+++ src/main/java/com/effortstone/backend/BackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing // 디폴드 값 설정할 때 넣어 줘야 하는 어노테이션
 public class BackendApplication {
 
     public static void main(String[] args) {
