39cdc6b1770fe7821a2813b249beaf514dc9888b
Wonhee <totohwh3601@naver.com>
2025-02-17 15:43:52 +0900

    Chore - Dto 데이터 타입 수정 - @Schema(example = "08:00:00")   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
      추가

--- src/main/java/com/effortstone/backend/domain/routine/dto/requset/RoutineRequestDto.java
+++ src/main/java/com/effortstone/backend/domain/routine/dto/requset/RoutineRequestDto.java
@@ -2,6 +2,7 @@ package com.effortstone.backend.domain.routine.dto.requset;
 
 import com.effortstone.backend.domain.routine.entity.RoutineGoalType;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -18,6 +19,7 @@ public class RoutineRequestDto {
     public static class RoutineCreateRequest {
         private String routineName;
         private RoutineGoalType routineGoalType;
+        @Schema(example = "08:00:00")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
         private LocalTime routineFocusTime;
         private List<Integer> routineRepeatFrequency;
@@ -25,10 +27,13 @@ public class RoutineRequestDto {
         private LocalDate routineEndDate;
         private String routineTheme;
         private String routineDetail;
+        @Schema(example = "08:00:00")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
         private LocalTime routineStartTime;
+        @Schema(example = "08:00:00")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
         private LocalTime routineEndTime;
+        @Schema(example = "08:00:00")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
         private LocalTime routineAlertTime;
     }
--- src/main/java/com/effortstone/backend/domain/todo/dto/response/TodoDto.java
+++ src/main/java/com/effortstone/backend/domain/todo/dto/response/TodoDto.java
@@ -1,5 +1,7 @@
 package com.effortstone.backend.domain.todo.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,9 +12,11 @@ import java.time.LocalTime;
 @Builder
 public class TodoDto {
     private Long todoCode; // todoCode
-    private String todoName; // 투두 시간
-    private LocalTime todoAlert; // 알람
+    private String todoName; // 투두 이름
+    @Schema(example = "08:00:00")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
+    private LocalTime todoAlert; // 알람 여부
     private LocalDate todoDate;    // 할일이 예정된 날짜
-    private String todoDetail;
-    private Boolean todoCompleted;
+    private String todoDetail; // 투두 메모
+    private Boolean todoCompleted; // 투두 성공 여부
 }
