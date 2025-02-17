687334abad6bbfa750b7f67a9cf22480c880ece9
Wonhee <totohwh3601@naver.com>
2025-02-17 14:06:22 +0900

    Chore - 주석 추가

--- src/main/java/com/effortstone/backend/global/common/response/SuccessCode.java
+++ src/main/java/com/effortstone/backend/global/common/response/SuccessCode.java
@@ -31,23 +31,26 @@ public enum SuccessCode {
     SEND_VERIFY_SUCCESS(OK, "휴대폰 인증번호 검증 성공"),
     USER_DAILY_AMOUNT_CHECK_SUCCESS(OK, "사용자 일일 이체 요금 조회 성공"),
     USER_GET_ALL_SUCCESS(OK,"모든사용자조회성공"),
-
-    // Product
     USER_CREATE_SUCCESS(CREATED, "유저생성 성공"), 
     USER_UPDATE_SUCCESS(OK, "유저정보 업데이트 성공"), 
     USER_DELETE_SUCCESS(OK, "유저정보 삭제 성공"),
     USER_CALENDER_SEARCH_SUCCESS(OK,"캘린더 정보조회 성공" ), 
+
+    // DIARY
     DIARY_SEARCH_ALL(OK,"다이어리전체조회" ),
     DIARY_DELETE_OK(OK,"다이어리삭제" ), 
     DIARY_SEARCH_CODE(OK,"특정 다이어리 조회 성공" ),
     DIARY_CREATE_OK(OK, "다이어리 생성 성공" ),
     DIARY_UPDATE_OK(OK,"다이어리 업데이트 성공" ),
+
+    // TODO
     TODO_LIST_FETCH_SUCCESS(OK, "투두 목록 조회 성공"),
     TODO_FETCH_SUCCESS(OK, "투두 조회 성공"),
     TODO_CREATE_SUCCESS(OK, "투두 생성 성공"),
     TODO_UPDATE_SUCCESS(OK, "투두 수정 성공"),
     TODO_DELETE_SUCCESS(OK, "투두 삭제 성공"),
 
+    // ROUTINE
     ROUTINE_LIST_FETCH_SUCCESS(OK, "루틴 목록 조회 성공"),
     ROUTINE_FETCH_SUCCESS(OK, "루틴 조회 성공"),
     ROUTINE_USER_LIST_FETCH_SUCCESS(OK, "사용자 루틴 목록 조회 성공"),
@@ -55,11 +58,14 @@ public enum SuccessCode {
     ROUTINE_UPDATE_SUCCESS(OK, "루틴 수정 성공"),
     ROUTINE_DELETE_SUCCESS(OK, "루틴 삭제 성공"),
     ROUTINE_CALENDAR_FETCH_SUCCESS(OK, "월간 캘린더 조회 성공"),
+
+    // STONE
     STONE_CREATE_SUCCESS(OK, "돌 생성 성공"),
     STONE_FETCH_SUCCESS(OK, "돌 조회 성공"),
     STONE_UPDATE_SUCCESS(OK, "돌 수정 성공"),
     STONE_DELETE_SUCCESS(OK, "돌 삭제 성공"),
 
+    // STONE_WEARABLE_ITEM
     STONE_WEARABLE_ITEM_ACQUIRE_SUCCESS(OK,"아이템 획득에 성공했습니다."),
     STONE_WEARABLE_ITEM_EQUIP_SUCCESS(OK,"아이템 장착에 성공했습니다."),
     STONE_WEARABLE_ITEM_UNEQUIP_SUCCESS(OK,"아이템 장착 해제에 성공했습니다."),
@@ -67,19 +73,21 @@ public enum SuccessCode {
     STONE_WEARABLE_ITEM_EQUIPPED_FETCH_SUCCESS(OK,"장착한 아이템 조회에 성공했습니다."),
     STONE_WEARABLE_ITEM_DELETE_SUCCESS(OK,"아이템 삭제에 성공했습니다."),
 
+    // ITEM
     ITEM_CREATE_SUCCESS(OK,"Item created successfully."),
     ITEM_FETCH_SUCCESS(OK,"Item fetched successfully."),
     ITEM_LIST_FETCH_SUCCESS(OK,"Item list fetched successfully."),
     ITEM_UPDATE_SUCCESS(OK,"Item updated successfully."),
     ITEM_DELETE_SUCCESS(OK,"Item deleted successfully."),
 
-    APP_INITIAL_FETCH_SUCCESS(OK,"초기정보조회에 성공했습니다.");
+    // APP
+    APP_INITIAL_FETCH_SUCCESS(OK,"초기정보조회에 성공했습니다."),
+
+    //SUBSCRIPTION
+    SUBSCRIPTION_GET_SUCCESS(OK,"상품 조회 성공했습니다." );
 
 
-    ;
 
-
-    ;
 
 
     private final HttpStatus httpStatus;
