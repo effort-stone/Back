dc0017ff8b773c0e24fc56e76b3679f6ffed4368
Wonhee <totohwh3601@naver.com>
2025-03-22 14:00:23 +0900

    :recycle: Refactor - dto에 fromEntity 추가

--- src/main/java/com/effortstone/backend/domain/useritem/dto/response/UserItemResponseDto.java
+++ src/main/java/com/effortstone/backend/domain/useritem/dto/response/UserItemResponseDto.java
@@ -1,6 +1,11 @@
 package com.effortstone.backend.domain.useritem.dto.response;
 
+import com.effortstone.backend.domain.user.dto.response.UserResponseDto;
+import com.effortstone.backend.domain.user.entity.User;
+import com.effortstone.backend.domain.useritem.entity.UserItem;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -8,4 +13,12 @@ import lombok.*;
 @AllArgsConstructor
 public class UserItemResponseDto {
     private Long userItemCode;
+    private LocalDateTime createdAt;
+
+    public static UserItemResponseDto fromEntity(UserItem userItem) {
+        return UserItemResponseDto.builder()
+                .userItemCode(userItem.getUserItemCode())
+                .createdAt(userItem.getCreatedAt())
+                .build();
+    }
 }
