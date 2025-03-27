b022928ca45177e63be9cca7eaa543486f81ba1f
Wonhee <totohwh3601@naver.com>
2025-03-27 21:20:51 +0900

    :sparkles: Feat - 결제검증코드 수정

--- src/main/java/com/effortstone/backend/global/auth/GooglePlayController.java
+++ src/main/java/com/effortstone/backend/global/auth/GooglePlayController.java
@@ -7,10 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -31,7 +28,7 @@ public class GooglePlayController {
      * @param googleDto 구매 토큰
      * @return 검증된 구매 정보(ProductPurchase 객체) 또는 오류 메시지
      */
-    @GetMapping("/verifyPurchase")
+    @PostMapping("/verifyPurchase")
     public ResponseEntity<?> verifyPurchase(
             GoogleDto googleDto) {
         try {
