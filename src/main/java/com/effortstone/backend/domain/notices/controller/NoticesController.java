a9b6a56c5920d833ea7f699f19a5a11dd6983d7d
Wonhee <totohwh3601@naver.com>
2025-04-01 22:51:58 +0900

    :memo: Memo - 수정

--- src/main/java/com/effortstone/backend/domain/notices/controller/NoticesController.java
+++ src/main/java/com/effortstone/backend/domain/notices/controller/NoticesController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/api/v1/notices")
+@RestController()
+@RequestMapping("/api/v1/notices")
 @RequiredArgsConstructor
 public class NoticesController {
 
