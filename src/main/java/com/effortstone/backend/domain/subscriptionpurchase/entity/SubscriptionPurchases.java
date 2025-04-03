9053cbfd8a4bc787a2250d7e79fb2f5af17e37c3
Wonhee <totohwh3601@naver.com>
2025-04-03 16:12:56 +0900

    :recycle: Refactor - orderId를 기본키로 설정 - user 추가

--- src/main/java/com/effortstone/backend/domain/subscriptionpurchase/entity/SubscriptionPurchases.java
+++ src/main/java/com/effortstone/backend/domain/subscriptionpurchase/entity/SubscriptionPurchases.java
@@ -2,6 +2,7 @@ package com.effortstone.backend.domain.subscriptionpurchase.entity;
 
 
 import com.effortstone.backend.domain.common.BaseEntity;
+import com.effortstone.backend.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -17,18 +18,14 @@ import java.time.LocalDateTime;
 @Setter
 public class SubscriptionPurchases extends BaseEntity {
 
+    // 주문 아이디
     @Id
-    @SequenceGenerator(name = "payment_seq", sequenceName = "payment_sequence", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "payment_seq")
-    @Column(name = "subscription_purchase_code")
-    private Long subscriptionPurchaseCode;
+    @Column(name = "order_id")
+    private String orderId;
 
     // 자동 결제 여부
     @Column(name = "auto_renewing")
     private Boolean autoRenewing;
-    // 주문 아이디
-    @Column(name = "order_id")
-    private String orderId;
     // 구독 시작일을 LocalDateTime으로 저장
     @Column(name = "start_time")
     private LocalDateTime startTime;
@@ -39,5 +36,7 @@ public class SubscriptionPurchases extends BaseEntity {
     @Column(name = "source")
     private String source;
 
-
+    @ManyToOne(fetch = FetchType.LAZY) // 성능 최적화를 위한 지연 로딩, 유저를 불러오는 코드를 작성하기 전까진 불러오지 않음
+    @JoinColumn(name = "user_code",nullable = false)
+    private User user;
 }
